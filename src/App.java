a1f7bd4af4e1a9b85b8f194a0090c5e64a4fdc7b
Arnaud Girard <girardarnaud111@gmail.com>
2023-10-16 18:17:46 -0400

    Add hex colorize()

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(AutoColor.colorize(true));
+        System.out.println(AutoColor.colorize("test", "#ffffff"));
     }
 }
