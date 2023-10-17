0eefc5a5bc3430df09f6bfb076d61220e8930719
Arnaud Girard <girardarnaud111@gmail.com>
2023-10-17 14:34:00 -0400

    add colorize() with delimiters

--- src/AutoColor.java
+++ src/AutoColor.java
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern;
+
 public class AutoColor {
     public static String colorize(boolean bool){
         return (bool ? "\033[1;32m" : "\033[1;31m") + bool + "\033[0m";
@@ -8,5 +10,22 @@ public class AutoColor {
         int green = Integer.valueOf(hexColor.substring(3, 5), 16);
         int blue = Integer.valueOf(hexColor.substring(5, 7), 16);
         return "\033[1;38;2;" + red + ";" + green + ";" + blue + "m" + str + "\033[0m";
+    }
+
+    public static String colorize(String str, char delimiter, String... hexColors) {
+        String[] strs = str.split(Pattern.quote(String.valueOf(delimiter)));
+        String finalStr = "";
+        int index = 0;
+        int hexIndex = 0;
+        while(index != strs.length) {
+            hexIndex = index;
+            if(hexIndex >= hexColors.length) hexIndex = hexColors.length - 1;
+            int red = Integer.valueOf(hexColors[hexIndex].substring(1, 3), 16);
+            int green = Integer.valueOf(hexColors[hexIndex].substring(3, 5), 16);
+            int blue = Integer.valueOf(hexColors[hexIndex].substring(5, 7), 16);
+            finalStr = finalStr + "\033[1;38;2;" + red + ";" + green + ";" + blue + "m" + strs[index] + "\033[0m";
+            index++;
+        }
+        return finalStr;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) throws Exception {
         System.out.println(AutoColor.colorize(true));
         System.out.println(AutoColor.colorize("test", "#f0f0f0"));
+        System.out.println(AutoColor.colorize("test|test|test", '|', "#eb3434", "#00fbff"));
     }
 }
